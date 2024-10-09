953703b056fee7f47e71016cc54b43add9d51792
minmise <dokhru@gmail.com>
2024-10-09 23:39:17 +0300

    add grid pane base for calendar

--- src/main/java/org/application/notecalendardesktop/CalendarWindow.java
+++ src/main/java/org/application/notecalendardesktop/CalendarWindow.java
@@ -2,6 +2,7 @@ package org.application.notecalendardesktop;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -10,6 +11,8 @@ import java.time.LocalDate;
 public class CalendarWindow {
 
     private final LocalDate today = LocalDate.now();
+    private final int WEEK_SIZE = 7;
+    private final int MONTH_SIZE = 6;
 
     public void build() {
         Stage stage = new Stage();
@@ -17,11 +20,17 @@ public class CalendarWindow {
         BorderPane bp = new BorderPane();
         Scene scene = new Scene(bp, 1320, 940);
         stage.setScene(scene);
-        Text text = new Text("Today is " +
-                today.getDayOfMonth() + "." +
-                today.getMonthValue() + "." +
-                today.getYear() + "!");
-        bp.setCenter(text);
+        GridPane gp_Calendar = new GridPane();
+        bp.setCenter(gp_Calendar);
+        for (int i = 0; i < MONTH_SIZE; ++i) {
+            for (int j = 0; j < WEEK_SIZE; ++j) {
+                Text text = new Text("Today is " +
+                        today.getDayOfMonth() + "." +
+                        today.getMonthValue() + "." +
+                        today.getYear() + "!");
+                gp_Calendar.add(text, i, j);
+            }
+        }
         stage.show();
     }
 
