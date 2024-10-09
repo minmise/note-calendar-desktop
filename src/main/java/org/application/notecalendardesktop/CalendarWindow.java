c2a14e9b72fdbf60212a0d7d8b3c525afd0edeb3
minmise <dokhru@gmail.com>
2024-10-09 23:24:34 +0300

    add today's date

--- src/main/java/org/application/notecalendardesktop/CalendarWindow.java
+++ src/main/java/org/application/notecalendardesktop/CalendarWindow.java
@@ -2,9 +2,14 @@ package org.application.notecalendardesktop;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.time.LocalDate;
+
 public class CalendarWindow {
+
+    private final LocalDate today = LocalDate.now();
 
     public void build() {
         Stage stage = new Stage();
@@ -12,6 +17,11 @@ public class CalendarWindow {
         BorderPane bp = new BorderPane();
         Scene scene = new Scene(bp, 1320, 940);
         stage.setScene(scene);
+        Text text = new Text("Today is " +
+                today.getDayOfMonth() + "." +
+                today.getMonthValue() + "." +
+                today.getYear() + "!");
+        bp.setCenter(text);
         stage.show();
     }
 
