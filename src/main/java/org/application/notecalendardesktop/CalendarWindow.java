f0ea61a1f6db6b5c7c7de50a949cf445ea1d45ee
minmise <dokhru@gmail.com>
2024-10-10 00:13:27 +0300

    add days of the week

--- src/main/java/org/application/notecalendardesktop/CalendarWindow.java
+++ src/main/java/org/application/notecalendardesktop/CalendarWindow.java
@@ -1,6 +1,7 @@
 package org.application.notecalendardesktop;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
@@ -13,6 +14,7 @@ public class CalendarWindow {
     private final LocalDate today = LocalDate.now();
     private final int WEEK_SIZE = 7;
     private final int MONTH_SIZE = 6;
+    private final String[] DAYS_OF_THE_WEEK = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
     public void build() {
         Stage stage = new Stage();
@@ -22,13 +24,17 @@ public class CalendarWindow {
         stage.setScene(scene);
         GridPane gp_Calendar = new GridPane();
         bp.setCenter(gp_Calendar);
-        for (int i = 0; i < MONTH_SIZE; ++i) {
+        for (int j = 0; j < WEEK_SIZE; ++j) {
+            Label dayLabel = new Label(DAYS_OF_THE_WEEK[j]);
+            gp_Calendar.add(dayLabel, j, 0);
+        }
+        for (int i = 1; i <= MONTH_SIZE; ++i) {
             for (int j = 0; j < WEEK_SIZE; ++j) {
                 Text text = new Text("Today is " +
                         today.getDayOfMonth() + "." +
                         today.getMonthValue() + "." +
                         today.getYear() + "!");
-                gp_Calendar.add(text, i, j);
+                gp_Calendar.add(text, j, i);
             }
         }
         stage.show();
