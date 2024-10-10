366e26193b322b4d1d69a5298060a2121bb5bf97
minmise <dokhru@gmail.com>
2024-10-10 03:23:09 +0300

    refactor

--- src/main/java/org/application/notecalendardesktop/CalendarWindow.java
+++ src/main/java/org/application/notecalendardesktop/CalendarWindow.java
@@ -11,40 +11,41 @@ import java.time.LocalDate;
 
 public class CalendarWindow {
 
-    private final LocalDate today = LocalDate.now();
-    private final int WEEK_SIZE = 7;
-    private final int MONTH_SIZE = 6;
-    private final String[] DAYS_OF_WEEK = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-    private final String[] MONTH_NAMES = {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    private static final int WEEK_SIZE = 7;
+    private static final int MONTH_SIZE = 6;
+    private static final String[] DAYS_OF_WEEK = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    private static final String[] MONTH_NAMES = {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
             "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-    private final String[] MONTH_NAMES_FULL = {"January", "February", "March", "April", "May", "June",
+    private static final String[] MONTH_NAMES_FULL = {"January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December"};
+
+    private final BorderPane bp = new BorderPane();
+    private final LocalDate MonthIterator = LocalDate.now().minusDays(LocalDate.now().getDayOfMonth() - 1);
     private final Label leftPointerLabel = new Label("<");
     private final Label rightPointerLabel = new Label(">");
-    private final Label curMonthLabel = new Label(MONTH_NAMES_FULL[today.getMonthValue() - 1] + ", " +
-            today.getYear());
 
-    public void build() {
-        Stage stage = new Stage();
-        stage.setTitle("Note Calendar");
-        BorderPane bp = new BorderPane();
-        Scene scene = new Scene(bp, 1320, 940);
-        stage.setScene(scene);
+    private Label getMonthLabel() {
+        return new Label(MONTH_NAMES_FULL[MonthIterator.getMonthValue() - 1] + ", " + MonthIterator.getYear());
+    }
+
+    private void rebuildCalendarByMonth() {
+        GridPane gp_Calendar = new GridPane();
         HBox hbTop = new HBox();
-        hbTop.getChildren().add(curMonthLabel);
         HBox hbBottom = new HBox();
+
+        hbTop.getChildren().add(getMonthLabel());
         hbBottom.getChildren().add(leftPointerLabel);
         hbBottom.getChildren().add(rightPointerLabel);
-        GridPane gp_Calendar = new GridPane();
         bp.setTop(hbTop);
         bp.setCenter(gp_Calendar);
         bp.setBottom(hbBottom);
+
         for (int j = 0; j < WEEK_SIZE; ++j) {
             Label dayLabel = new Label(DAYS_OF_WEEK[j]);
             gp_Calendar.add(dayLabel, j, 0);
         }
-        LocalDate startOfMonth = today.minusDays(today.getDayOfMonth() - 1);
-        LocalDate curDay = startOfMonth.minusDays(startOfMonth.getDayOfWeek().getValue() - 1);
+
+        LocalDate curDay = MonthIterator.minusDays(MonthIterator.getDayOfWeek().getValue() - 1);
         for (int i = 1; i <= MONTH_SIZE; ++i) {
             for (int j = 0; j < WEEK_SIZE; ++j) {
                 int dayOfMonth = curDay.getDayOfMonth();
@@ -57,6 +58,14 @@ public class CalendarWindow {
                 curDay = curDay.plusDays(1);
             }
         }
+    }
+
+    public void build() {
+        Stage stage = new Stage();
+        Scene scene = new Scene(bp, 1320, 940);
+        stage.setTitle("Note Calendar");
+        stage.setScene(scene);
+        rebuildCalendarByMonth();
         stage.show();
     }
 
