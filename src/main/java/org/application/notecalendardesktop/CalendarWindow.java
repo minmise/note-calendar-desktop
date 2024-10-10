a3c5443443bd8cec8524d5c0b2f32eee1dd29273
minmise <dokhru@gmail.com>
2024-10-10 15:12:14 +0300

    make "plus" button that create label in vbox in gridpane of calendar

--- src/main/java/org/application/notecalendardesktop/CalendarWindow.java
+++ src/main/java/org/application/notecalendardesktop/CalendarWindow.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class CalendarWindow {
@@ -24,8 +25,10 @@ public class CalendarWindow {
     private final BorderPane bp = new BorderPane();
     private final Label leftPointerLabel = new Label("<");
     private final Label rightPointerLabel = new Label(">");
+    private final ArrayList<ArrayList<VBox>> vBoxList = new ArrayList<>();
 
     private LocalDate monthIterator = LocalDate.now().minusDays(LocalDate.now().getDayOfMonth() - 1);
+    GridPane gp_Calendar = null;
 
     private Label getMonthLabel() {
         Label l = new Label(MONTH_NAMES_FULL[monthIterator.getMonthValue() - 1] + ", " + monthIterator.getYear());
@@ -33,8 +36,23 @@ public class CalendarWindow {
         return l;
     }
 
+    private Label createPlusLabel(int x, int y) {
+        Label l = new Label("+");
+        l.setStyle(vBoxList.get(x).get(y).getStyle());
+        l.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            Label l_new = new Label(".");
+            l_new.setStyle(vBoxList.get(x).get(y).getStyle());
+            vBoxList.get(x).get(y).getChildren().removeLast();
+            vBoxList.get(x).get(y).getChildren().add(l_new);
+            vBoxList.get(x).get(y).getChildren().add(createPlusLabel(x, y));
+            gp_Calendar.getChildren().remove(vBoxList.get(x).get(y));
+            gp_Calendar.add(vBoxList.get(x).get(y), y, x + 1);
+        });
+        return l;
+    }
+
     private void rebuildCalendarByMonth() {
-        GridPane gp_Calendar = new GridPane();
+        gp_Calendar = new GridPane();
         HBox hbTop = new HBox();
         HBox hbBottom = new HBox();
 
@@ -51,7 +69,10 @@ public class CalendarWindow {
 
         for (int j = 0; j < WEEK_SIZE; ++j) {
             Label dayLabel = new Label(DAYS_OF_WEEK[j]);
-            dayLabel.setStyle("-fx-background-color: #ffbebe;-fx-min-height: 30;-fx-font-size: 20pt;");
+            dayLabel.setStyle("-fx-background-color: #ffbebe;" +
+                    "-fx-min-height: 30;" +
+                    "-fx-font-size: 20pt;" +
+                    "-fx-min-width: 150;");
             gp_Calendar.add(dayLabel, j, 0);
         }
 
@@ -59,19 +80,26 @@ public class CalendarWindow {
         int counter = 0;
         for (int i = 1; i <= MONTH_SIZE; ++i) {
             for (int j = 0; j < WEEK_SIZE; ++j) {
+                VBox box = vBoxList.get(i - 1).get(j);
+                box.getChildren().clear();
                 int dayOfMonth = curDay.getDayOfMonth();
                 Label dateLabel = new Label(String.valueOf(dayOfMonth));
                 if (dayOfMonth == 1) {
                     ++counter;
                 }
+                box.setStyle("-fx-background-color: #ffffff;");
                 dateLabel.setStyle("-fx-alignment: top-left;");
                 if (counter != 1) {
+                    box.setStyle("-fx-background-color: #c0c0c0;-fx-alignment: top-left;");
                     dateLabel.setStyle("-fx-background-color: #c0c0c0;-fx-alignment: top-left;");
                 } else if (dayOfMonth == LocalDate.now().getDayOfMonth() &&
                         monthIterator.getMonthValue() == LocalDate.now().getMonthValue()) {
                     dateLabel.setStyle("-fx-text-fill: red;-fx-font-size: 20pt;-fx-alignment: top-left;");
                 }
-                VBox box = new VBox(dateLabel);
+                box.getChildren().add(dateLabel);
+                box.getChildren().add(createPlusLabel(i - 1, j));
+                box.setMinWidth(150);
+                box.setMinHeight(100);
                 gp_Calendar.add(box, j, i);
                 curDay = curDay.plusDays(1);
             }
@@ -103,6 +131,12 @@ public class CalendarWindow {
             monthIterator = monthIterator.plusMonths(1);
             rebuildCalendarByMonth();
         });
+        for (int i = 0; i < MONTH_SIZE; ++i) {
+            vBoxList.add(new ArrayList<>());
+            for (int j = 0; j < WEEK_SIZE; ++j) {
+                vBoxList.get(i).add(new VBox());
+            }
+        }
         rebuildCalendarByMonth();
         stage.show();
     }
