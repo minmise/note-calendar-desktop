a0ff7873d6cd5826fd216585e314febdcf4b1e45
minmise <dokhru@gmail.com>
2024-10-11 03:23:50 +0300

    create note class

---
+++ src/main/java/org/application/notecalendardesktop/client/Note.java
@@ -0,0 +1,21 @@
+package org.application.notecalendardesktop.client;
+
+import java.time.LocalDate;
+
+public class Note {
+
+    private final LocalDate localDate = null;
+
+    private String name = null;
+    private String description = null;
+    private boolean isTimeIncluded = false;
+    private int hours = 0;
+    private int minutes = 0;
+    /* private boolean isRegular = false;
+    private int yearRegularity = 0;
+    private int monthRegularity = 0;
+    private int dayRegularity = 0;
+    private int hourRegularity = 0;
+    private int minuteRegularity = 0; */
+
+}
